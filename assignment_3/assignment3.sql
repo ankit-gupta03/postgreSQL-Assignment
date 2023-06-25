88aaea45db6572e6622aea84411cfc39120b4c3d
ankit-gupta03 <ankit.caizin03@gmail.com>
2023-06-25 18:00:49 +0530

    Assignment3.sql

--- assignment_3/assignment3.sql
+++ assignment_3/assignment3.sql
@@ -1,4 +1,4 @@
--- Query 1
+--1
 
 create or replace procedure enroll_into_tournament(member_name VARCHAR(20),tournament_name VARCHAR(30))
 language plpgsql
@@ -18,7 +18,7 @@ select * from tournament_entry
 select * from tournament
 select * from members
 
--- Query 2
+--2
 
 create or replace function tournament(in tour_year int,out no_of_tournament int)  
 language plpgsql  
@@ -32,7 +32,7 @@ select * from tournament(2022);
 select * from tournament_entry;
 select * from tournament
 
--- Query 3
+--3
 
 create or replace function count_tournament_year(tour_year int) 
 returns table (t_name varchar,t_type varchar,is_open boolean, country varchar)  
@@ -40,15 +40,17 @@ language plpgsql
 as 
 $$  
 begin 
-    return query select tournament.tour_name,tournament.tour_type,tournament.is_open,tournament.country
-	from tournament inner join tournament_entry on tournament.tour_id = tournament_entry.tournament_id where tournament_entry.tournament_year = tour_year;
+    return query select tournament.tour_name,tournament.tour_type,
+	tournament.is_open,tournament.country
+	from tournament inner join tournament_entry on
+	tournament.tour_id = tournament_entry.tournament_id where tournament_entry.tournament_year = tour_year;
 end;  
 $$;  
 select * from count_tournament_year(2023);
 select * from tournament_entry
 select * from tournament
 
--- Query 4
+--4
 
 create or replace procedure manager_change(team_name varchar(20))
 language plpgsql
@@ -64,16 +66,17 @@ call manager_change('Slytherine');
 select * from members;
 select * from team
 
--- Query 5
+--5
 
 create or replace function team_details(team_name varchar) 
-returns table(member_firstname varchar,member_lastname varchar,membershiptype varchar,joindate date,gender varchar)  
+returns table(member_firstname varchar,member_lastname varchar,
+	membershiptype varchar,joindate date,gender varchar)  
 language plpgsql
 as  
 $$  
 begin
-    return query select members.firstname,members.lastname,member_type.member_type,members.join_date,members.gender
-	from members inner join member_type on members.member_type_id = member_type.member_typeid
+    return query select members.firstname,members.lastname,member_type.member_type,
+	members.join_date,members.gender from members inner join member_type on members.member_type_id = member_type.member_typeid
 	inner join team on members.team_id = team.team_id
 	where team.teamname = team_name;
 end;  
@@ -85,7 +88,7 @@ select * from team;
 select * from members;
 select * from member_type;
 
--- Query 6
+--6
 
 create or replace procedure coach_change(coach_name varchar(20), member_name varchar(20))
 language plpgsql
@@ -106,7 +109,7 @@ call coach_change('Babu','Rohit');
 select * from members;
 
 
--- Query 7
+--7
 
 create or replace function year_wise_participants(in tour_year int,out count_participants int)
 language plpgsql
@@ -119,7 +122,3 @@ $$;
 select * from year_wise_participants(2023);
 select * from tournament
 select * from tournament_entry
-select * from members
-
-
-
